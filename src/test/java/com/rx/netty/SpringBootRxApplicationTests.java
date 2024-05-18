9c1217058298d9cbe33a7e099ed0b8f06eaa79ba
manzoDev <manzoroger08@gmail.com>
2024-05-18 11:20:22 -0400

    first commit

---
+++ src/test/java/com/rx/netty/SpringBootRxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rx.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
